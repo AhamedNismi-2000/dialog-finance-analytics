4ffd852cd60070ff10a44049531150df6ab2b011
Ahamed Nismi <mnanismi2000@gmail.com>
2026-05-16 17:07:34 +0530

    data cleaning

--- PythonScript/python_scripts/02_load_table_postgres.py
+++ PythonScript/python_scripts/02_load_table_postgres.py
@@ -4,9 +4,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from pathlib import Path
 
-
 # 1. Load environment variables
-
 load_dotenv()
 
 user = os.getenv("DB_USER")
@@ -15,31 +13,32 @@ host = os.getenv("DB_HOST")
 port = os.getenv("DB_PORT")
 db = os.getenv("DB_NAME")
 
-# Debug (REMOVE later in production)
-print("DB USER:", user)
-print("DB PASSWORD:", password)
-
-
-# 2. Build database connection
-
+# 2. Create database connection
 engine = create_engine(
     f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
 )
 
-
-# 3. Load Excel file safely
-
-BASE_DIR = Path(__file__).resolve().parent.parent
+# 3. Load Excel file
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 file_path = BASE_DIR / "Data" / "raw_data" / "FintechTrainee.xlsx"
 
 df = pd.read_excel(file_path)
 
-print("Excel loaded successfully!")
+print("Excel loaded successfully")
 print(df.head())
 
+# 4. Convert column names in to appropriate format 
 
-# 4. Load data into PostgreSQL
+df.columns = (
+    df.columns
+    .str.strip()               # remove spaces
+    .str.lower()               # convert to lowercase
+    .str.replace(" ", "_")     # replace spaces with _
+)
 
+print("Cleaned columns:", df.columns)
+
+# 5. Load into PostgreSQL
 table_name = "fintech"
 
 df.to_sql(
--- SQL/01_clean_data.sql
+++ SQL/01_clean_data.sql
@@ -1,2 +1,25 @@
+ SELECT * FROM fintech
 
-    SELECT * FROM fintech
+  -- remove the duplicate values
+
+--         SELECT customer_id, COUNT(*)
+--         FROM fintech
+--         GROUP BY customer_id
+--         HAVING COUNT(*) > 1;
+
+--   WITH cte AS (
+--     SELECT *,
+--            ROW_NUMBER() OVER (
+--                PARTITION BY customer_id
+--            ) AS rn
+--     FROM fintech
+-- )
+--     DELETE FROM fintech
+--     WHERE customer_id IN (
+--         SELECT customer_id
+--         FROM cte
+--         WHERE rn > 1
+-- );
+
+
+SELECT district,province FROM fintech
